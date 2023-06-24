82a2eef67bec18b09572c347aaa5ff105d78fde1
Mitchell Scott <Mitch-scott@outlook.com>
2023-06-23 22:40:35 -0500

    added placeholder text and bs

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
+import 'bootswatch/dist/flatly/bootstrap.min.css';
+import { useEffect } from "react";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />
 }
