2c22883c11954a6997832244651606139d345913
Mitchell Scott <Mitch-scott@outlook.com>
2023-06-23 23:07:09 -0500

    wip

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
+      <main className="container d-flex justify-content-center">
         <h1>Hey</h1>
         <div>
           This is a placeholder site. <br />
