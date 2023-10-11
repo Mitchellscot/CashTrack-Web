f64a177a7394e3e51892495654b380aed82c33c5
Mitchell Scott <mitchellscott@me.com>
2023-10-10 21:27:00 -0500

    wip

---
+++ app/components/Feature/Feature.tsx
@@ -0,0 +1,8 @@
+import classNames from 'classnames';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Feature.module.scss';
+
+export default function Feature() {
+  return <></>;
+}
